aab08f5cf9d0c6bea08905e3f841b692a3caf222
jmunozpe <jmunozpe@unal.edu.co>
2025-10-21 19:35:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from shapes import Rectangle, Circle, Triangle
 
-# Crear objetos
+
 rect = Rectangle(4, 6)
 circle = Circle(3)
 tri = Triangle(base=4, height=3, side_a=3, side_b=4, side_c=5)
 
-# Mostrar resultados
+
 print(rect)
 print()
 print(circle)
