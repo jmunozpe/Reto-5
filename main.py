3a08e0afa91b4cb9cbbab47d58dbc0561dee71d7
jmunozpe <jmunozpe@unal.edu.co>
2025-10-21 19:41:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ print()
 print(circle)
 print()
 print(tri)
+
+from module_shapes.shapes import Rectangle, Circle, Triangle
+
+rect = Rectangle(5, 7)
+circle = Circle(3)
+tri = Triangle(base=4, height=3, side_a=3, side_b=4, side_c=5)
+
+print(rect)
+print()
+print(circle)
+print()
+print(tri)
