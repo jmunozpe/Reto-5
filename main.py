40b02bc07d6165d8c1cc2a0a00be7fc08a3449fc
jmunozpe <jmunozpe@unal.edu.co>
2025-10-21 19:51:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+# ======================================================
+# el paquete con módulos separados (shapes/)
+# ======================================================
+
 from shapes import Rectangle, Circle, Triangle
 
+print("=== Ejecución usando paquete shapes/ ===\n")
 
 rect = Rectangle(4, 6)
 circle = Circle(3)
 tri = Triangle(base=4, height=3, side_a=3, side_b=4, side_c=5)
 
-
 print(rect)
 print()
 print(circle)
 print()
 print(tri)
 
-from module_shapes.shapes import Rectangle, Circle, Triangle
+# ======================================================
+# el paquete con módulo único (module_shapes/)
+# ======================================================
 
-rect = Rectangle(5, 7)
-circle = Circle(3)
-tri = Triangle(base=4, height=3, side_a=3, side_b=4, side_c=5)
+from module_shapes.shapes import Rectangle as ModRectangle, Circle as ModCircle, Triangle as ModTriangle
 
-print(rect)
+print("\n\n=== Ejecución usando paquete module_shapes/ ===\n")
+
+rect2 = ModRectangle(5, 7)
+circle2 = ModCircle(3)
+tri2 = ModTriangle(base=4, height=3, side_a=3, side_b=4, side_c=5)
+
+print(rect2)
 print()
-print(circle)
+print(circle2)
 print()
-print(tri)
+print(tri2)
