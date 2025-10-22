71dc7f4c53dad930af6c5fe2632f0599577f2c0e
jmunozpe <jmunozpe@unal.edu.co>
2025-10-21 19:08:37 -0500

    Create rectangle.py

---
+++ shapes/shapes/rectangle.py
@@ -0,0 +1,12 @@
+from .base import Shape
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
