ff9b17c76d9965e037cfd372beb9607cd0113fe6
jmunozpe <jmunozpe@unal.edu.co>
2025-10-21 19:34:35 -0500

    Update circle.py

--- shapes/circle.py
+++ shapes/circle.py
@@ -5,8 +5,17 @@ class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
 
-    def area(self):
-        return pi * (self.radius ** 2)
+    def compute_area(self):
+        return pi * self.radius ** 2
 
-    def perimeter(self):
+    def compute_perimeter(self):
         return 2 * pi * self.radius
+
+    def __str__(self):
+        return (
+            f"Circle:\n"
+            f"  Radius: {self.radius}\n"
+            f"  Area: {self.compute_area():.2f}\n"
+            f"  Circumference: {self.compute_perimeter():.2f}"
+        )
+
